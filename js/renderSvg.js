c57490e0193a957828e7114ab1877e7b029ed2a5
ginkomix <accer96226@gmail.com>
2018-01-18 17:57:46 +0300

    svg low

--- js/renderSvg.js
+++ js/renderSvg.js
@@ -7,18 +7,19 @@ const RenderSvg = (RenderSvg) => class extends RenderSvg {
 
     drawRect() {
 
-        let svg = document.querySelector('#svgGame');
+        let svg = document.querySelector('#svgGame'),
+            svgns = "http://www.w3.org/2000/svg";
         svg.style.width = this.width+'%';
         svg.style.height = this.height+'%';
         for(let i =0;i<this.x;i++) {   
             for(let j =0;j<this.y;j++) {
-                let rect = document.createElement('rect');
-                rect.className ='col'+ i;
-                rect.className +=' row'+ j;
+                let rect = document.createElementNS(svgns,'rect');
+                rect.setAttribute('class','col'+ i); 
+                rect.setAttribute('class',' row'+ j);
                 rect.setAttribute('x',j*this.square);
                 rect.setAttribute('y',i*this.square);
-                rect.style.width=this.square+'px';
-                 rect.style.height=this.square+'px';
+                rect.setAttribute('width',this.square);
+                rect.setAttribute('height',this.square);
                 svg.appendChild(rect);
             }
 
